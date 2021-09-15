0db3348a092cdf0c5c2fd2b7e8ef6d631f3e005e
Alex12345678902 <56139315+Phos202@users.noreply.github.com>
2021-09-15 15:12:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    import java.util.Scanner;
+
+      Scanner scan = new Scanner(System.in);
+       /* Write your code here */
+      System.out.println("Please enter a three digit number:");
+      
+      int a = scan.nextInt();
+      
+      int ab = a;
+      
+      int ac = a;
+      
+      
+      
+      
+      
+      
+      System.out.println("Here are the digits");
+      
+      System.out.println(ac % 670);
+     
+      System.out.println(ab);
+     
+      System.out.println(a / 100);
+
+
+
+  }
+}
